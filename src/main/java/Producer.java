4ff1f7e8cdc3bc4b090b348b474eaf25f7af73f4
httung12303 <92967099+httung12303@users.noreply.github.com>
2022-09-30 00:15:57 +0700

    Producer sends messages until SHUTDOWN message

--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -4,22 +4,27 @@ import com.rabbitmq.client.Connection;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 
 public class Producer {
   public static void main(String[] args) throws IOException, TimeoutException {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("localhost");
-//    factory.setUsername("admin");
-//    factory.setPassword("admin");
     try (Connection conn = factory.newConnection()) {
       Channel channel = conn.createChannel();
       channel.queueDeclare("hello-world", false, false, false, null);
-
-      String message = "Hello world!";
-
-      channel.basicPublish("", "hello-world", null, message.getBytes(StandardCharsets.UTF_8));
-      System.out.println("Message sent");
+      Scanner scanner = new Scanner(System.in);
+      while (true) {
+        System.out.print("Enter message: ");
+        String message = scanner.nextLine();
+        channel.basicPublish("", "hello-world", false, false, null, message.getBytes());
+        System.out.println("Message sent successfully!");
+        if (message.equalsIgnoreCase("SHUTDOWN")) {
+          break;
+        }
+      }
+      channel.close();
     }
   }
 }
