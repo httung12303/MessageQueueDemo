d196abe600c5fd630eedcdd4aa0c0ae97b995f69
httung12303 <92967099+httung12303@users.noreply.github.com>
2022-09-30 00:16:58 +0700

    Add Consumer

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,24 @@
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer {
+  public static void main(String[] args) throws IOException, TimeoutException {
+    ConnectionFactory factory = new ConnectionFactory();
+    Connection conn = factory.newConnection();
+    Channel channel = conn.createChannel();
+    channel.queueDeclare("hello-world", false, false, false, null);
+    channel.queueDeclare("hello-world", false, false, false, null);
+    channel.basicConsume(
+        "hello-world",
+        true,
+            (s, delivery) -> {
+              String message = new String(delivery.getBody());
+              System.out.println("Message received: " + message);
+            },
+        consumerTag -> {});
+    channel.close();
+    conn.close();
+  }
+}
